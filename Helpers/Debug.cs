07b1a6b469551b7cdc38262d262cc45c5d4798d8
someC0d3r <ke.may.business@gmail.com>
2022-01-07 12:17:24 +0100

    Added functionality to throw debug messages

---
+++ Helpers/Debug.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Dynamics.Nav.CodeAnalysis.Diagnostics;
+
+namespace BusinessCentral.LinterCop.Helpers {
+    public class Debug {
+
+        public static void ThrowDebugDiagnostic(ref SymbolAnalysisContext context, string id, string message) {
+            DiagnosticDescriptor DebugDescriptor =
+                new DiagnosticDescriptor(
+                    id,
+                    message,
+                    message,
+                    "Design",
+                    DiagnosticSeverity.Info,
+                    true,
+                    message);
+
+            context.ReportDiagnostic(Diagnostic.Create(DebugDescriptor,context.Symbol.Location,new object[] { }));
+        }
+
+        public static void ThrowDebugDiagnostic(ref SymbolAnalysisContext context, string id, string message, Microsoft.Dynamics.Nav.CodeAnalysis.Text.Location location) {
+            DiagnosticDescriptor DebugDescriptor =
+                new DiagnosticDescriptor(
+                    id,
+                    message,
+                    message,
+                    "Design",
+                    DiagnosticSeverity.Info,
+                    true,
+                    message);
+
+            context.ReportDiagnostic(Diagnostic.Create(DebugDescriptor, location, new object[] { }));
+        }
+    }
+}
